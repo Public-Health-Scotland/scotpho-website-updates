d386578a72db0a487dff136950667c6c3d0e4bb2
Jaime Villacampa <jaime.villacampa@nhs.net>
2019-09-03 14:45:49 +0100

    comments, tidying, simplify creating chart files

--- Asthma.R
+++ Asthma.R
@@ -1,15 +1,18 @@
 # Code to analyse asthma incidence and asthma deaths data from SMRA for publication on scotPHO website
 # current analysis for september 2019 website update
 
+# Part 1 - deaths file - data from SMRA
+# Part 2 - Extract data from SMRA on asthma admissions
+# Part 3 - Calculate incidence rates and export files
+
+###############################################.
+# Functions/packages/filepaths ----
+###############################################.
 # load packages required to run all commands
 library(tidyr)
-library(foreign) 
 library(dplyr)
-library(ggplot2) 
-library(RcppRoll) 
 library(readr) 
 library(odbc) 
-library(readxl)
 
 # file path for saved files
 data_folder <- "/PHI_conf/ScotPHO/Website/Topics/Asthma/sept2019_update/"
@@ -30,6 +33,20 @@ create_rates <- function(dataset, epop_total) {
            rate = easr*100000)  # rate calculation
 }
 
+# Function to create the files required for updating the charts
+create_chart_data <- function(dataset, epop_total, filename) {
+  asthma_rates <- create_rates(dataset = dataset, epop_total = epop_total)
+  
+  # export in format for website chart update (year, sex, rate in csv file) and save
+  asthma_rates <- asthma_rates %>% select(year, sex, rate) %>% 
+    mutate(sex = recode(sex, "1" = "Male", "2" = "Female"),
+           year = paste0(asthma_rates$year, "/", substr(asthma_rates$year+1, 3,4)))
+  
+  asthma_chart <<- asthma_rates #to allow checking
+  
+  write_csv(asthma_rates, paste0(data_folder, filename , ".csv"))
+}
+
 # SMRA login information
 channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
                                       uid=.rs.askForPassword("SMRA Username:"), 
@@ -38,17 +55,18 @@ channel <- suppressWarnings(dbConnect(odbc(),  dsn="SMRA",
 ###############################################.
 # Part 1 - deaths file - data from SMRA ----
 ###############################################.
-
-# SQL query for asthma deaths
+# SQL query for asthma deaths: Scottish residents with a main cause of death of asthma
+# extracting by date of registration and getting both calendar and financial year
 asthma_deaths <- tbl_df(dbGetQuery(channel, statement=
   "SELECT LINK_NO linkno, YEAR_OF_REGISTRATION cal_year, UNDERLYING_CAUSE_OF_DEATH cod, AGE, SEX, DATE_OF_registration doadm,
-        CASE WHEN extract(month from DATE_OF_registration) > 3 
-         THEN extract(year from DATE_OF_registration)
-  ELSE extract(year from DATE_OF_registration) -1 END as year
+        CASE WHEN extract(month from date_of_registration) > 3 
+          THEN extract(year from date_of_registration)
+          ELSE extract(year from date_of_registration) -1 END as year
     FROM ANALYSIS.GRO_DEATHS_C
-    WHERE date_of_death between '1 January 2002' and '31 December 2018'
-    AND country_of_residence ='XS'
-             AND (substr(UNDERLYING_CAUSE_OF_DEATH,0,3) = any('J45','J46', '493') or substr(UNDERLYING_CAUSE_OF_DEATH,0,4) = '-493')")) %>%
+    WHERE date_of_registration between '1 January 2002' and '31 December 2018'
+        AND country_of_residence ='XS'
+        AND (substr(UNDERLYING_CAUSE_OF_DEATH,0,3) = any('J45','J46', '493') 
+            or substr(UNDERLYING_CAUSE_OF_DEATH,0,4) = '-493')")) %>%
   setNames(tolower(names(.)))  # variables to lower case
 
 # recode age groups
@@ -63,40 +81,42 @@ asthma_deaths <- asthma_deaths %>% mutate(age_grp = case_when(
 ))
 
 # calculate the number of deaths (EASR not required for deaths data on scotpho website)
-asthma_deaths_total <- asthma_deaths %>% group_by(sex, year) %>% 
+asthma_deaths_chart <- asthma_deaths %>% group_by(sex, cal_year) %>% 
   count() %>% # calculate numerator
   ungroup()
 
 # export in format for website chart update (year, sex, rate in csv file) and save
-asthma_deaths_chart <- asthma_deaths_total %>% select(year, sex, n) %>% 
+asthma_deaths_chart <- asthma_deaths_chart %>% select(cal_year, sex, n) %>% 
   mutate(sex = recode(sex, "1" = "Male", "2" = "Female"))
 
-asthma_deaths_chart_tot <- asthma_deaths_chart %>% group_by(year) %>% 
- summarise(n =sum(n)) %>%  mutate(sex = "All")
-
-asthma_deaths_chart <- rbind(asthma_deaths_chart, asthma_deaths_chart_tot)
+# Calculating deaths for all gender and adding them to the data by gender
+asthma_deaths_chart <- asthma_deaths_chart %>% group_by(cal_year) %>% 
+ summarise(n =sum(n)) %>%  mutate(sex = "All") %>% 
+  rbind(., asthma_deaths_chart)
 
 write.csv(asthma_deaths_chart, file=paste0(data_folder, "asthma_deaths_chart.csv"))
 
 ###############################################.
-# Part 2 - Extract data from SMRA ----
+# Part 2 - Extract data from SMRA on asthma admissions ----
 ###############################################.
-
 # SQL query extracts data one row per admission with an asthma diagnosis, by financial year. 
 # Excluding unvalid sex cases and non-scottish
 query_sql <- function(table) {
-  paste0("SELECT distinct link_no linkNo, cis_marker CIS, max(age_in_years) age, min(ADMISSION_DATE) doadm, 
-    max(sex) sex, max(CASE WHEN extract(month from discharge_date) > 3 
-         THEN extract(year from discharge_date)
-         ELSE extract(year from discharge_date) -1 END) as year
+  paste0("SELECT distinct link_no linkNo, cis_marker CIS, max(age_in_years) age, 
+                min(ADMISSION_DATE) doadm, max(sex) sex, 
+                max(CASE WHEN extract(month from discharge_date) > 3 
+                    THEN extract(year from discharge_date)
+                    ELSE extract(year from discharge_date) -1 END) as year
          FROM ", table,
          " WHERE discharge_date between '1 April 1992' and '31 March 2019' 
-         AND hbtreat_currentdate is not null
-         AND substr(hbtreat_currentdate,0,4) != 'S082'
-         AND sex in ('1','2')
-         AND (substr(main_condition,0,3) = any('J45','J46', '493') or substr(main_condition,0,4) = '-493')
+              AND hbtreat_currentdate is not null
+              AND substr(hbtreat_currentdate,0,4) != 'S082'
+              AND sex in ('1','2')
+              AND (substr(main_condition,0,3) = any('J45','J46', '493') 
+                  OR substr(main_condition,0,4) = '-493')
          GROUP BY link_no, cis_marker")
 }
+
 # Doing query for both historical records and more recent ones and joining together
 data_asthma <- rbind(tbl_df(dbGetQuery(channel, statement= query_sql("ANALYSIS.SMR01_PI"))),
                      tbl_df(dbGetQuery(channel, statement= query_sql("ANALYSIS.SMR01_HISTORIC"))) ) %>%
@@ -117,11 +137,11 @@ deaths_admissions <- deaths_admissions %>% mutate(age_grp = case_when(
   age_grp2 = case_when(age < 10 ~ 1, age > 9 ~ 2, TRUE ~ as.numeric(age)
   ))
 
-#10year lookback Calculate lookback-
+# Calculating 10 year lookback 
 deaths_admissions <- deaths_admissions %>%
   arrange(linkno, doadm) %>% 
   group_by(linkno) %>% 
-  # calculating difference between first admission and each one of them. Converting into years
+  # calculating difference between each admission and its previous one in years
   mutate(diff_time = as.numeric(difftime(doadm, lag(doadm), units="days"))/365) %>%
   # select first admission/death per person with no previous admission, within 10 years
   filter(is.na(diff_time) | diff_time >= 10) %>%
@@ -155,61 +175,34 @@ scottish_population <- scottish_population %>% mutate(age_grp = case_when(
 deaths_admissions_scotland <- deaths_admissions %>% group_by(age_grp, age_grp2, sex, year) %>% 
   count() # calculate numerator
 
+# Joining data with population (denominator)
 deaths_admissions_scotland <- full_join(deaths_admissions_scotland, scottish_population, c("year", "age_grp", "sex")) %>% 
   rename(numerator = n, denominator = pop) # numerator and denominator used for calculation
 
 deaths_admissions_scotland <- deaths_admissions_scotland %>% 
-  mutate(epop = recode(as.character(age_grp), 
+  mutate(epop = recode(as.character(age_grp), # EASR age group pops
                        "1"=5000, "2"=5500, "3"=5500, "4"=5500, "5"=6000, 
                        "6"=6000, "7"= 6500, "8"=7000, "9"=7000, "10"=7000,
                        "11"=7000, "12"=6500, "13"=6000, "14"=5500, "15"=5000,
-                       "16"= 4000, "17"=2500, "18"=1500, "19"=1000)) # EASR age group pops
+                       "16"= 4000, "17"=2500, "18"=1500, "19"=1000)) 
 
-deaths_admissions_scotland$numerator[is.na(deaths_admissions_scotland$numerator)] <- 0 # Converting NA's to 0s
+# Converting NA's to 0s
+deaths_admissions_scotland$numerator[is.na(deaths_admissions_scotland$numerator)] <- 0 
 
 # this section is for splitting ages under 10 and 10+ for males and females
-data_agegroups <- deaths_admissions_scotland %>% group_by(year, age_grp2, sex) %>% select(-age_grp) %>% 
-  summarise_all(funs(sum), na.rm =T) %>% rename(age_grp = age_grp2)
+data_agegroups <- deaths_admissions_scotland %>% group_by(year, age_grp2, sex) %>% 
+  select(-age_grp) %>% 
+  summarise_all(list(sum), na.rm =T) %>% rename(age_grp = age_grp2)
 
 data_underten <- data_agegroups %>% filter(age_grp == 1) # under 10
 data_tenplus <- data_agegroups %>% filter(age_grp == 2) # 10 plus
 
-# run the create rates function for male/female all
-asthma_scotland <- create_rates(dataset = deaths_admissions_scotland, epop_total = 100000) # 100000 used because split into males and females (total would be 200000)
-
+# run the create rates function for each cut
 # export in format for website chart update (year, sex, rate in csv file) and save
-asthma_scotland_chart <- asthma_scotland %>% select(year, sex, rate) %>% 
-  mutate(sex = recode(sex, "1" = "Male", "2" = "Female"),
-         year = paste0(asthma_scotland$year, "/", substr(asthma_scotland$year+1, 3,4)))
+create_chart_data(dataset = deaths_admissions_scotland, epop_total = 100000, filename = "asthma_scotland_chart")
+create_chart_data(dataset = data_underten, epop_total = 10500, filename = "asthma_underten_chart")
+create_chart_data(dataset = data_tenplus, epop_total = 89500, filename = "asthma_tenplus_chart")
 
-write.csv(asthma_scotland_chart, file=paste0(data_folder, "asthma_scotland_chart.csv"))
-
-# run the create rates function for male/female under 10
-asthma_underten <- create_rates(dataset = data_underten, epop_total = 10500) # 10500 is the total for people under 10
-
-# export in format for website chart update (year, sex, rate in csv file) and save
-asthma_underten_chart <- asthma_underten %>% select(year, sex, rate) %>% 
-  mutate(sex = recode(sex, "1" = "Male", "2" = "Female"),
-         year = paste0(asthma_underten$year, "/", substr(asthma_underten$year+1, 3,4)))
-
-write.csv(asthma_underten_chart, file=paste0(data_folder, "asthma_underten_chart.csv"))
-
-# run the create rates function for male/female 10+
-asthma_tenplus <- create_rates(dataset = data_tenplus, epop_total = 89500) # 89500 is the total for people over 10
-
-# export in format for website chart update (year, sex, rate in csv file) and save
-asthma_tenplus_chart <- asthma_tenplus %>% select(year, sex, rate) %>% 
-  mutate(sex = recode(sex, "1" = "Male", "2" = "Female"),
-         year = paste0(asthma_tenplus$year, "/", substr(asthma_tenplus$year+1, 3,4)))
-
-write.csv(asthma_tenplus_chart, file=paste0(data_folder, "asthma_tenplus_chart"))
-
-
-
-
-           
-  
+##END
 
   
-
-
