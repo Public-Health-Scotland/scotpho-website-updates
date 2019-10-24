799a58d9ea1db6d313540b8135782009449b6460
Jaime Villacampa <jaime.villacampa@nhs.net>
2019-10-24 15:11:23 +0100

    making agegroup function more readable, tidying

--- 1.analysis_functions.R
+++ 1.analysis_functions.R
@@ -35,27 +35,6 @@ create_rates <- function(dataset, epop_total, sex ) {
            rate = easr*100000)  # rate calculation
 }
 
-# functions used in analysis
-create_rates <- function(dataset, epop_total) { #use function to calculate for each allergy later
-  dataset <- dataset %>% 
-    mutate(epop = recode(as.character(age_grp), 
-                         "1"=5000, "2"=5500, "3"=5500, "4"=5500, "5"=6000, 
-                         "6"=6000, "7"= 6500, "8"=7000, "9"=7000, "10"=7000,
-                         "11"=7000, "12"=6500, "13"=6000, "14"=5500, "15"=5000,
-                         "16"= 4000, "17"=2500, "18"=1500, "19"=1000)) %>% #EASR age group pops 
-    mutate(easr_first = numerator*epop/denominator) #easr population
-  
-  # aggregating by year, code and time
-  dataset <- dataset %>% subset(select= -c(age_grp)) %>%
-    group_by(year, sex) %>% summarise_all(funs(sum), na.rm =T) %>% ungroup()
-  
-  #Calculating rates
-  dataset <- dataset %>%
-    mutate(epop_total = epop_total,  # Total EPOP population
-           easr = easr_first/epop_total, # easr calculation
-           rate = easr*100000)  # rate calculation
-}
-
 ###############################################.
 # Function to create the files required for updating the charts
 create_chart_data <- function(dataset, epop_total, filename, sex = T, year_type = "financial") {
@@ -84,16 +63,14 @@ create_chart_data <- function(dataset, epop_total, filename, sex = T, year_type 
 # Function to create age groups
 # recode age groups
 create_agegroups <- function(dataset) {
-  dataset <- dataset %>% mutate(age_grp = case_when( 
-    age < 5 ~ 1, age > 4 & age <10 ~ 2, age > 9 & age <15 ~ 3, age > 14 & age <20 ~ 4,
-    age > 19 & age <25 ~ 5, age > 24 & age <30 ~ 6, age > 29 & age <35 ~ 7, 
-    age > 34 & age <40 ~ 8, age > 39 & age <45 ~ 9, age > 44 & age <50 ~ 10,
-    age > 49 & age <55 ~ 11, age > 54 & age <60 ~ 12, age > 59 & age <65 ~ 13, 
-    age > 64 & age <70 ~ 14, age > 69 & age <75 ~ 15, age > 74 & age <80 ~ 16,
-    age > 79 & age <85 ~ 17, age > 84 & age <90 ~ 18, age > 89 ~ 19, 
-    TRUE ~ as.numeric(age)
-  ))
-}
+    dataset %>% mutate(age_grp = case_when(between(age, 0, 4) ~ 1,
+      between(age, 5, 9) ~ 2, between(age, 10, 15) ~ 3, between(age, 15, 19) ~ 4, 
+      between(age, 20, 24) ~ 5, between(age, 25, 29) ~ 6, between(age, 30, 34) ~ 7, 
+      between(age, 35, 39) ~ 8, between(age, 40, 44) ~ 9, between(age, 45, 49) ~ 10, 
+      between(age, 50, 54) ~ 11, between(age, 55, 59) ~ 12, between(age, 60, 64) ~ 13,
+      between(age, 65, 69) ~ 14, between(age, 70, 74) ~ 15,  between(age, 75, 79) ~ 16,
+      between(age, 80, 84) ~ 17, between(age, 85, 89) ~ 18, between(age, 90, 200) ~ 19))
+  }
 
 ###############################################.
 # Add European Population for rate calculation
