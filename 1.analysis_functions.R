b755f5f228cf1bbbca84eba1b0c0fa9d2d156281
Jaime Villacampa <jaime.villacampa@nhs.net>
2019-10-29 13:56:50 +0000

    typo in age group function

--- 1.analysis_functions.R
+++ 1.analysis_functions.R
@@ -64,7 +64,7 @@ create_chart_data <- function(dataset, epop_total, filename, sex = T, year_type 
 # recode age groups
 create_agegroups <- function(dataset) {
     dataset %>% mutate(age_grp = case_when(between(age, 0, 4) ~ 1,
-      between(age, 5, 9) ~ 2, between(age, 10, 15) ~ 3, between(age, 15, 19) ~ 4, 
+      between(age, 5, 9) ~ 2, between(age, 10, 14) ~ 3, between(age, 15, 19) ~ 4, 
       between(age, 20, 24) ~ 5, between(age, 25, 29) ~ 6, between(age, 30, 34) ~ 7, 
       between(age, 35, 39) ~ 8, between(age, 40, 44) ~ 9, between(age, 45, 49) ~ 10, 
       between(age, 50, 54) ~ 11, between(age, 55, 59) ~ 12, between(age, 60, 64) ~ 13,
